05f93c335740b66062f58385c9c8923102a49402
Peter Bludau <bludau@fortiss.org>
2019-07-06 12:05:58 +0200

    init

--- node_helper.js
+++ node_helper.js
@@ -35,29 +35,10 @@ module.exports = NodeHelper.create({
     		.observeDevices();
 		} catch (e) {
 			// handle error - see below for details
-			console.log("TRADFRI INIT FAILED")
+			console.log("TRADFRI INIT FAILED");
 		}
-		self.gpio = []
-		for (let index = 0; index < pinConfigs.length; ++index) {
-			let pinConfig = pinConfigs[index];
-          	self.gpio[String(pinConfig.pin)] = new Gpio(pinConfig.pin, pinConfig.direction);
-		}
-	  }
-	  self.started = true;				
 	}
-    else if (notification === 'TOGGLE_PIN') {     
-		let pinNumber = payload;	  
-		let pin = self.gpio[String(pinNumber)];
-		let value = pin.readSync();
-		if (value !== 1) {
-			value = 1;
-		}
-		else{
-			value = 0;
-		}
-		pin.writeSync(value);
-		console.log(`Pin ${pinNumber} switched to ${value}`);
-    };
+	}
   }
 });
   	
